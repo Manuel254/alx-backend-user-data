e1c206c8af473767512119f1f8170ef170ba7849
Manuel254 <immanuelkituku@gmail.com>
2023-08-03 07:14:07 +0000

    Fix variable

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -39,11 +39,11 @@ def get_db() -> mysql.connector.connection.MySQLConnection:
     host = os.environ.get('PERSONAL_DATA_DB_HOST', 'localhost')
     db_name = os.environ.get('PERSONAL_DATA_DB_NAME')
 
-    connection = mysql.connector.connection.MySQLConnection(host=host,
+    conn = mysql.connector.connection.MySQLConnection(host=host,
                                                             database=db_name,
                                                             user=username,
                                                             password=password)
-    return connection
+    return conn
 
 
 def main():

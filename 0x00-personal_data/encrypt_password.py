2c2a82745720bf4a4939d45f9da6bfa5799a1c81
Manuel254 <immanuelkituku@gmail.com>
2023-08-03 07:11:04 +0000

    Validate password against hash

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -7,3 +7,11 @@ def hash_password(password: str) -> bytes:
     """Returns a salted hashed password"""
     password = bytes(password, encoding='utf-8')
     return bcrypt.hashpw(password, bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """Checks if a password is valid"""
+    password = bytes(password, encoding='utf-8')
+    if bcrypt.checkpw(password, hashed_password):
+        return True
+    return False

2838ba52ef133db0c300866698844edccf20efe8
Manuel254 <immanuelkituku@gmail.com>
2023-08-10 08:47:09 +0000

    Add authentication based on session id stored in cookie

---
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""Handles all routes for session authentication"""
+from flask import request, abort, jsonify
+from api.v1.views import app_views
+from models.user import User
+from os import getenv
+
+
+@app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
+def login():
+    """Handles login for user"""
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    if email == '' or email is None:
+        return jsonify({"error": "email missing"}), 400
+    if password == '' or password is None:
+        return jsonify({"error": "password missing"}), 400
+
+    users = User.search(dict(email=email))
+
+    if not users or len(users) == 0:
+        return jsonify({"error": "no user found for this email"}), 404
+    else:
+        for user in users:
+            if user.is_valid_password(password):
+                from api.v1.app import auth
+                session_id = auth.create_session(user.id)
+                session_name = getenv('SESSION_NAME')
+
+                res = jsonify(user.to_json())
+                res.set_cookie(session_name, session_id)
+                return res
+        return jsonify({"error": "wrong password"}), 401

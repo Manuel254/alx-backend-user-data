b304ab26c77216705d5b57a678465c5e417411a5
Manuel254 <immanuelkituku@gmail.com>
2023-08-17 19:31:32 +0000

    Logout route

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -49,7 +49,7 @@ def login():
 @app.route('/sessions', methods=['DELETE'], strict_slashes=False)
 def logout():
     """Logout route for user"""
-    session_id = request.cookie.get('session_id')
+    session_id = request.cookies.get('session_id')
 
     user = AUTH.get_user_from_session_id(session_id)
 

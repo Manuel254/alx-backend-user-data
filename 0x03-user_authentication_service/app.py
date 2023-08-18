186c10c3ac017e102041678a8d68995dcc789d71
Manuel254 <immanuelkituku@gmail.com>
2023-08-18 10:51:16 +0000

    User profile

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -60,5 +60,20 @@ def logout():
         abort(403)
 
 
+@app.route('/profile', strict_slashes=False)
+def profile():
+    """Handles user profile"""
+    try:
+        session_id = request.cookies.get('session_id')
+        user = AUTH.get_user_from_session_id(session_id)
+
+        if user:
+            return jsonify({"email": user.email}), 200
+        else:
+            raise Exception
+    except Exception:
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
